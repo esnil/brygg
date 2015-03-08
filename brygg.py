d2c2eae969eeba580b826a4988d9f136dcf322fa
Anders Musikka <anders.musikka@gmail.com>
2015-03-08 22:17:36 +0100

    Update

--- brygg.py
+++ brygg.py
@@ -6,16 +6,33 @@ from flask import make_response
 
 import traceback
 import StringIO
-
+import json
 from werkzeug.routing import BaseConverter
 
 app = Flask(__name__)
+coords=[]
 
 @app.route('/')
 def root():
     return render_template('index.html')
     
+@app.route(r'/save',methods=["POST"])
+def save():
     
+    coordinates=dict()
+    for key in request.form.keys():
+        value=request.form[key]
+        axis,ordinal=re.match(r"(x|y)_(\d+)",key)
+        axisnum=1 if axis=='y' else 0
+        coordinates.setdefault(int(ordinal),[0,0])[axisnum]=float(value)
+    
+    coords=[]
+    for coord in sorted(coordinates.items(),key=lambda x:x[0]):
+        coords.append(coord)
+    print coords
+    
+    return json.dumps('')
+
        
 if __name__ == '__main__':    
     app.debug=True
